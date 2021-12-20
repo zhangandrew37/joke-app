f64d621980e4909cbd682a1717c5ba58b4e28d06
zhangandrew37 <zhangandrew37@gmail.com>
2021-12-20 14:06:31 -0500

    add migration

---
+++ jokes-web-app/Data/Migrations/20211220190401_initialsetup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace jokes_web_app.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Joke",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    JokeQuestion = table.Column<string>(type: "TEXT", nullable: true),
+                    JokeAnswer = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Joke", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Joke");
+        }
+    }
+}
